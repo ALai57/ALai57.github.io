9a435e85fa73d70e5554e55c78e60fccf7ffdd7a
ALai57 <andrew.s.lai5@gmail.com>
2016-09-13 00:47:38 -0500

    Add files via upload
    First upload.

---
+++ basicStyleSetup.php
@@ -0,0 +1,16 @@
+
+
+<?php
+	
+	$baseSite    = "http://localhost/MyTutorial";
+	$styleFolder = "/Styling"; 
+	
+	echo
+"<link rel=\"stylesheet\" type=\"text/css\" href=\"" .$baseSite .$styleFolder . "/mystyle.css\">
+<link rel=\"stylesheet\" type=\"text/css\" href=\"" .$baseSite .$styleFolder . "/navstyle.css\">
+<link rel=\"stylesheet\" type=\"text/css\" href=\"" .$baseSite .$styleFolder . "/sidebarstyle.css\">
+<link rel=\"stylesheet\" type=\"text/css\" href=\"" .$baseSite .$styleFolder . "/articleheaderstyle.css\">
+
+<link rel=\"icon\" href=\"" .$baseSite . "\Favicon.png\" type=\"image/png\" />";
+
+?>
---
+++ index.php
@@ -0,0 +1,20 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Home</title>
+<?php include 'basicStyleSetup.php'; ?>
+</head>
+<body>
+<?php include 'Styling/navbar.php'; ?>
+<h1 id="article-title">Home page</h1>
+<div class="textbody">
+<p>This is my personal web page. Still under construction.</p> 
+</div>
+<div id="sidebar">
+<ul class="sidelink">Here are some links:
+	<li><a href="#Link1">Link 1</a></li>
+	<li><a href="#Link2">Link 2</a></li>
+	<li><a href="#Link3">Link 3</a></li>
+</ul>
+</div>
+</body>
+</html>
